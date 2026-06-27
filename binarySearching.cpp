7cd1c2c18d7a5a6dc44d3fd30a794b695b3313fb
Abhay Yadav <abhayyadav19955@gmail.com>
2026-06-27 16:27:19 +0530

    27/06/2026

--- binarySearching.cpp
+++ binarySearching.cpp
@@ -2,26 +2,34 @@
 using namespace std;
 
 int main(){
-    vector <int> arr={1,2,3,4,5,6,7,8,9};
+
+    vector<int> arr(1000);
+    iota(arr.begin(), arr.end(), 1); // Fills 1 to 1000
+
     int s, e;
+
     s = 0;
     e = arr.size()-1;
     int target;
+
     cout << "Enter target: ";
     cin >> target;
-    cout << s << " and " << e << endl;
-    int mid = (s+e)/2;
 
-    if(arr[mid]==target){
-        cout << "Target " << target << " is found" << endl;
+    while(s<=e){
+
+        int mid = (s+e)/2;
+
+        if(arr[mid]==target){
+            cout << "Target: " << target << " is found at: " << mid+1 << endl;
+            break;
+        }
+        else if(arr[mid]>target){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
     }
-    else if(arr[mid]>target){
-        e=mid-1;
-    }
-    else{
-        s=mid+1;
-    }
-    cout << s << " and " << e;
 
     return 0;
 }
