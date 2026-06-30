bf40a3a0b125cb9673d606e5306b0e3160b28209
Abhay Yadav <abhayyadav19955@gmail.com>
2026-06-30 12:45:14 +0530

    30/06/2026

--- strings6LowercaseToUppercase.cpp
+++ strings6LowercaseToUppercase.cpp
@@ -5,6 +5,15 @@ using namespace std;
 #define mod 1000000007
 
 int main(){
-    
+    string strLower;
+    string strUpper;
+    cout << "Enter a Word in lowercase: ";
+    cin >> strLower;
+    for (int i = 0; i < strLower.size(); i++){
+        if(strLower[i]>='a' && strLower[i]<='z'){
+            strUpper.push_back(strLower[i]-32);
+        }
+    }
+    cout << strUpper;
     return 0;
 }
